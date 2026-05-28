acb9d5d6cab37519707ed53ecc21f9e18ee88851
ushabiradar <ushabiradar614@gmail.com>
2026-05-28 14:21:23 +0530

    work on

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,21 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { TestComponent } from './shared/components/test/test.component';
+import { AuthComponent } from './shared/components/auth/auth.component';
+import { GetConfirmComponent } from './shared/components/get-confirm/get-confirm.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TestComponent,
+    AuthComponent,
+    GetConfirmComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/shared/components/get-confirm/get-confirm.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-get-confirm',
+  templateUrl: './get-confirm.component.html',
+  styleUrls: ['./get-confirm.component.scss']
+})
+export class GetConfirmComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
